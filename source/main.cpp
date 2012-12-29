52acef77ddc99d9d7396012bbb5b5900422a3977
Ivan Kornilov <vano468@gmail.com>
2012-12-29 12:09:42 +0400

    Hello World with SingleTouch (Initial)

---
+++ source/main.cpp
@@ -0,0 +1,41 @@
+#include "s3e.h"
+#include "IwGx.h"
+
+struct CTouchb{
+    int32 x;
+    int32 y;
+    bool active;
+    int32 id;
+} g_Touches;
+
+void SingleTouchMotionCB(s3ePointerMotionEvent* event) {
+    g_Touches.x = event->m_x;
+    g_Touches.y = event->m_y;
+}
+
+void SingleTouchButtonCB(s3ePointerEvent* event) {
+    g_Touches.active = event->m_Pressed != 0;
+    g_Touches.x = event->m_x;
+    g_Touches.y = event->m_y;
+}
+
+int main() {
+    IwGxInit();
+    IwGxSetColClear(0, 0, 0xff, 0xff);
+        
+    while(!s3eDeviceCheckQuitRequest()
+          && !(s3eKeyboardGetState(s3eKeyEsc) & S3E_KEY_STATE_DOWN)
+          && !(s3eKeyboardGetState(s3eKeyAbsBSK) & S3E_KEY_STATE_DOWN)
+          ) {
+        IwGxClear();
+
+		s3ePointerRegister(S3E_POINTER_BUTTON_EVENT, (s3eCallback)SingleTouchButtonCB, NULL);
+        IwGxPrintString(g_Touches.x, g_Touches.y, "Hello, World!");
+
+        IwGxFlush();
+        IwGxSwapBuffers();
+        s3eDeviceYield(0);	
+    }
+    IwGxTerminate();
+    return 0;
+}
